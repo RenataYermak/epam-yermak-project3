2ffe9a4551a149556b14536837ddf5ea4207d32e
RenataYermak <renatkahturo@gmail.com>
2021-10-29 14:27:22 +0300

    .

--- src/main/java/by/epam/yermak/task2/main/ConsoleRunning.java
+++ src/main/java/by/epam/yermak/task2/main/ConsoleRunning.java
@@ -1,17 +1,27 @@
 package by.epam.yermak.task2.main;
 
 import by.epam.yermak.task1.exception.ArrayIndexOutOfBoundsException;
+
 import by.epam.yermak.task2.entity.JaggedArray;
+import by.epam.yermak.task2.entity.SortType;
 import by.epam.yermak.util.ScannerUtil;
 
 public class ConsoleRunning {
-    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
-        int size = ScannerUtil.scanner.nextInt();
-        JaggedArray jaggedArray = new JaggedArray(size);
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size-1; j++) {
-                jaggedArray.add(ScannerUtil.scanner.nextInt());
+    public static void main(String[] args) {
+        int n = ScannerUtil.scanner.nextInt();
+        int[][] matrix = new int[n][];
+        for (int i = 0; i < n; i++) {
+            matrix[i] = new int[i + 1];
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ScannerUtil.scanner.nextInt();
             }
         }
+        JaggedArray jaggedArray = new JaggedArray(matrix);
+        jaggedArray.sort(SortType.ROW_SUM_ASCENDING);
+        jaggedArray.sort(SortType.ROW_SUM_DESCENDING);
+        jaggedArray.sort(SortType.MAX_ELEMENT_ASCENDING);
+        JaggedArray.sort(SortType.MAX_ELEMENT_DESCENDING);
+        JaggedArray.sort(SortType.MIX_ELEMENT_ASCENDING);
+        JaggedArray.sort(SortType.MIX_ELEMENT_DESCENDING);
     }
 }
--- src/main/java/by/epam/yermak/task2/service/IArrayService.java
+++
@@ -1,8 +0,0 @@
-package by.epam.yermak.task2.service;
-
-public interface IArrayService {
-    void bubbleSort();
-    int getMinElementRow();
-    int getMaxElementRow();
-    int rowSum();
-}
